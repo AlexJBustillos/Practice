9ad377b73f16e69961511c5f1bdd51110f8e4942
alexjbustillos <alexjbustillos@gmail.com>
2021-04-20 13:12:43 -0400

    max char

--- cardio.js
+++ cardio.js
@@ -73,3 +73,26 @@ function capitalizeLetters(str) {
 
     return str.replace(/\b[a-z]/gi, (char) => char.toUpperCase());
 }
+
+function maxCharacter(str) {
+    const charMap = {};
+    let maxNum = 0;
+    let maxChar = '';
+
+    str.split('').forEach((char) => {
+        if (charMap[char]) {
+            charMap[char]++;
+        } else {
+            charMap[char] = 1;
+        }
+    })
+
+    for(let char in charMap) {
+        if(charMap[char] > maxNum) {
+            maxNum = charMap[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar
+}
