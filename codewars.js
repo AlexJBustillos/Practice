167f384d3fd97399e4e21d56af9af0d54154743c
alexjbustillos <alexjbustillos@gmail.com>
2021-04-26 15:33:43 -0400

    isPangram

--- codewars.js
+++ codewars.js
@@ -1,14 +1,21 @@
-const solution = (number) => {
-    for (i = 0; i <= number.length; i++) {
-        if (number % 3 === 0) {
-            return number++
-        } else if (number % 5 === 0) {
-            return number++
-        } else {
-            return 0
-        }
-    }
-    return number
+// const solution = (number) => {
+//     for (i = 0; i <= number.length; i++) {
+//         if (number % 3 === 0 || number % 5 === 0) {
+//             return number++
+//         } else {
+//             return 0
+//         }
+//     }
+//     return number
+// }
+
+// console.log(solution(10));
+
+//////////////////////////////////////////////////////////////
+
+
+const isPangram = (string) => {
+    return string.match(/[^a-zA-Z]/g) ? true : false;
 }
 
-console.log(solution(10));
+console.log(isPangram("The quick brown fox jumps over the lazy dog."));
