e8f8339872f6a610c6d70954c481e15db851c3c6
alexjbustillos <alexjbustillos@gmail.com>
2021-04-27 12:51:54 -0400

    isPangram solved

--- codewars.js
+++ codewars.js
@@ -15,7 +15,7 @@
 
 
 const isPangram = (string) => {
-    return string.match(/[^a-zA-Z]/g) ? true : false;
+    return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
 }
 
 console.log(isPangram("The quick brown fox jumps over the lazy dog."));
