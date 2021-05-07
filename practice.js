f5bb5bfd3b5bc9e80a8e1459e0abc168bf6e6632
alexjbustillos <alexjbustillos@gmail.com>
2021-05-06 20:49:07 -0400

    practice problems

--- practice.js
+++ practice.js
@@ -1,14 +1,30 @@
 // How do you find the duplicate number on a given array
 
-const duplicateNumber = (arr) => {
-    for (i = 0; i < arr.length; i++) {
-        if (i === i) return i
-        else i += 1
+// const duplicateNumber = (arr) => {
+//     for (i = 0; i < arr.length; i++) {
+//         if (i === i) return i
+//         else i += 1
+//     }
+//     return i
+// }
+
+// arr = [1, 1, 2, 5, 6, 9, 10]
+
+// duplicateNumber(arr)
+// const selectDiv = document.getElementById("practice")
+// selectDiv.addEventListener('keydown', divListener)
+// function divListener(e) {
+//         selectDiv.textContent += `${e.code}`
+// }
+
+// function isEmpty( el ){
+//     return !$.trim(el.html())
+// }
+function emptyHeader() {
+    if ($(":header").html().length === 0 ) {
+        $(":header").attr("role", "presentation")
     }
-    return i
+
 }
 
-arr = [1, 1, 2, 5, 6, 9, 10]
-
-duplicateNumber(arr)
-
+// $(":header").is($(":empty"))
