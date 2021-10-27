b1337e46c53fcd4e780a28cd247e18cb1587801e
alexjbustillos <alexjbustillos@gmail.com>
2021-10-26 20:51:32 -0400

    codewars tictactoe

--- practice.js
+++ practice.js
@@ -242,3 +242,41 @@ const points = (games) => {
     return acc;
   }, 0);
 }
+
+
+const isSolved = (board) => {
+  function isWinner(bo, le) {
+    if (
+      (bo[0][0] === le && bo[0][1] === le && bo[0][2] === le) ||
+      (bo[1][0] === le && bo[1][1] === le && bo[1][2] === le) ||
+      (bo[2][0] === le && bo[2][1] === le && bo[2][2] === le) ||
+      (bo[0][0] === le && bo[1][0] === le && bo[2][0] === le) ||
+      (bo[0][1] === le && bo[1][1] === le && bo[2][1] === le) ||
+      (bo[0][2] === le && bo[1][2] === le && bo[2][2] === le) ||
+      (bo[0][0] === le && bo[1][1] === le && bo[2][2] === le) ||
+      (bo[0][2] === le && bo[1][1] === le && bo[2][0] === le)
+    ) {
+      return true;
+    } else { return false; }
+  }
+
+  function emptySpots(bo) {
+    let empty = false;
+    bo.forEach( arr1 => {
+      arr1.forEach( arr2 => {
+        if (arr2 === 0) { empty = true ;}
+      })
+    })
+    return empty;
+  }
+
+  if (isWinner(board,1)) {
+    return 1;
+  } else if (isWinner(board, 2)) {
+    return 2;
+  } else if (emptySpots(board)) {
+    return -1;
+  } else {
+    return 0
+  }
+}
